269d690c766d489c4f84a04ceac3bd98d6535c97
Katerina Savidaki <ksavvidaki@gmail.com>
2025-04-16 21:02:21 +0300

    feat: components created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Katerina';
+
+  person = {
+    givenName: 'Katerina',
+    surname: 'Savidaki',
+    age: 29,
+    email: 'ksavvidaki@aueb.gr'
+  }
 }
