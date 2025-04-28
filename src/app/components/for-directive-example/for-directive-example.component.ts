7028a0252aaa3df63931356b6de1fd3d43588399
Katerina Savidaki <ksavvidaki@gmail.com>
2025-04-28 19:10:52 +0300

    feat: component examples and functions added

--- src/app/components/for-directive-example/for-directive-example.component.ts
+++ src/app/components/for-directive-example/for-directive-example.component.ts
@@ -1,11 +1,135 @@
 import { Component } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
+import { PersonTableComponent } from "../person-table/person-table.component";
 
 @Component({
   selector: 'app-for-directive-example',
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './for-directive-example.component.html',
   styleUrl: './for-directive-example.component.css'
 })
 export class ForDirectiveExampleComponent {
-
+  users: Person[] = [
+        {
+          "givenName": "Melamie",
+          "surname": "Rapi",
+          "email": "mrapi0@mysql.com",
+          "age": 1,
+          "address": "PO Box 25676"
+        }, {
+          "givenName": "Veda",
+          "surname": "Kemwall",
+          "email": "vkemwall1@typepad.com",
+          "age": 2,
+          "address": "Room 331"
+        }, {
+          "givenName": "Welbie",
+          "surname": "Haysman",
+          "email": "whaysman2@nsw.gov.au",
+          "age": 3,
+          "address": "Room 35"
+        }, {
+          "givenName": "Jacques",
+          "surname": "Wingfield",
+          "email": "jwingfield3@flavors.me",
+          "age": 4,
+          "address": "Room 1273"
+        }, {
+          "givenName": "Ave",
+          "surname": "Leek",
+          "email": "aleek4@mysql.com",
+          "age": 5,
+          "address": "Apt 761"
+        }, {
+          "givenName": "Goddard",
+          "surname": "Tixall",
+          "email": "gtixall5@admin.ch",
+          "age": 6,
+          "address": "Suite 75"
+        }, {
+          "givenName": "Rinaldo",
+          "surname": "Meaddowcroft",
+          "email": "rmeaddowcroft6@msu.edu",
+          "age": 7,
+          "address": "Room 1353"
+        }, {
+          "givenName": "Bram",
+          "surname": "Airy",
+          "email": "bairy7@weebly.com",
+          "age": 8,
+          "address": "PO Box 49416"
+        }, {
+          "givenName": "Leanna",
+          "surname": "Apfler",
+          "email": "lapfler8@indiatimes.com",
+          "age": 9,
+          "address": "1st Floor"
+        }, {
+          "givenName": "Shel",
+          "surname": "Vazquez",
+          "email": "svazquez9@instagram.com",
+          "age": 10,
+          "address": "1st Floor"
+        }, {
+          "givenName": "Nolly",
+          "surname": "Rottery",
+          "email": "nrotterya@bravesites.com",
+          "age": 11,
+          "address": "Suite 41"
+        }, {
+          "givenName": "Jimmy",
+          "surname": "Leagas",
+          "email": "jleagasb@flickr.com",
+          "age": 12,
+          "address": "Room 1312"
+        }, {
+          "givenName": "Trude",
+          "surname": "Koppes",
+          "email": "tkoppesc@gnu.org",
+          "age": 13,
+          "address": "Room 1954"
+        }, {
+          "givenName": "Orville",
+          "surname": "McCaskell",
+          "email": "omccaskelld@independent.co.uk",
+          "age": 14,
+          "address": "Suite 50"
+        }, {
+          "givenName": "Jayne",
+          "surname": "Lenz",
+          "email": "jlenze@artisteer.com",
+          "age": 15,
+          "address": "PO Box 85027"
+        }, {
+          "givenName": "Ceciley",
+          "surname": "Boutcher",
+          "email": "cboutcherf@cpanel.net",
+          "age": 16,
+          "address": "Apt 197"
+        }, {
+          "givenName": "Salomon",
+          "surname": "Leftwich",
+          "email": "sleftwichg@51.la",
+          "age": 17,
+          "address": "Room 1707"
+        }, {
+          "givenName": "Ariel",
+          "surname": "Gentner",
+          "email": "agentnerh@dagondesign.com",
+          "age": 18,
+          "address": "11th Floor"
+        }, {
+          "givenName": "Marleen",
+          "surname": "Gorcke",
+          "email": "mgorckei@feedburner.com",
+          "age": 19,
+          "address": "Suite 44"
+        }, {
+          "givenName": "Torey",
+          "surname": "Goulstone",
+          "email": "tgoulstonej@pinterest.com",
+          "age": 20,
+          "address": "PO Box 19970"
+        }
+  ]
 }
