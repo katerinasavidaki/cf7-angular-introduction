576fa8748aa8c9c14d35a8968506f02ee25284ed
Katerina Savidaki <ksavvidaki@gmail.com>
2025-04-28 18:32:54 +0300

    feat: angular navbar

--- src/app/components/component-input-example/component-input-example.component.ts
+++ src/app/components/component-input-example/component-input-example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-component-input-example',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './component-input-example.component.css'
 })
 export class ComponentInputExampleComponent {
+  person0: Person = {
+    givenName: 'Christodoulos',
+    surname: 'Fragkoudakis',
+    age: 55,
+    email: 'chfrag@aueb.gr',
+    address: 'New York, USA',
+  }
 
+  person1: Person = {
+    givenName: 'John',
+    surname: 'Doe',
+    age: 32,
+    email: 'john@aueb.gr',
+    address: 'Athens, Greece',
+  }
 }
